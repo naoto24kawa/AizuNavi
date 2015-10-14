23deaf63ba8bdaa008cb3d72ac6098079d0c2d3b
Naoto Nishikawa <naoto24kawa@naoto24kawa.local>
2015-10-15 01:59:54 +0900

    first commmit

---
+++ app/src/main/java/com/naoto24kawa/aizunavi/activities/MapActivity.java
@@ -0,0 +1,14 @@
+package com.naoto24kawa.aizunavi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
