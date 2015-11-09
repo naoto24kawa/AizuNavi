1aca0048ef32ddbf4629c7b9467ca5313fab2705
Naoto Nishikawa <naoto24kawa@naoto24kawa.local>
2015-11-10 01:20:28 +0900

    modify map preference

--- app/src/main/java/com/naoto24kawa/aizunavi/models/Consts.java
+++ app/src/main/java/com/naoto24kawa/aizunavi/models/Consts.java
@@ -4,4 +4,7 @@ import java.util.Locale;
 
 public class Consts {
     public static Locale LOCALE = Locale.getDefault();
+
+    public static final double AIZUWAKAMATSU_LAT = 37.494863;
+    public static final double AIZUWAKAMATSU_LNG = 139.929725;
 }
