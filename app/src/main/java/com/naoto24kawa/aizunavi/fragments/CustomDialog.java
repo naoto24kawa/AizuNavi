2e457883b4b7889973043188633822b2632ac5fd
Naoto Nishikawa <naoto24kawa@naoto24kawa.local>
2015-11-12 14:55:00 +0900

    add root search func

---
+++ app/src/main/java/com/naoto24kawa/aizunavi/fragments/CustomDialog.java
@@ -0,0 +1,7 @@
+package com.naoto24kawa.aizunavi.fragments;
+
+/**
+ * Created by naoto24kawa on 15/11/10.
+ */
+public class CustomDialog {
+}
---
+++ app/src/main/java/com/naoto24kawa/aizunavi/network/GoogleDirectionAPIPerser.java
@@ -0,0 +1,7 @@
+package com.naoto24kawa.aizunavi.network;
+
+/**
+ * Created by naoto24kawa on 15/11/11.
+ */
+public class GoogleDirectionAPIPerser {
+}
